c005e801ede0a3e38e75e8a99f1831ed787ef869
laurentbatailley <laurent.batailley@gmail.com>
2017-02-06 23:02:13 +0100

    gulp, wbabebabel, wp2

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,82 @@
-var gulp   = require('gulp'),
-    watch      = require('gulp-watch'),
-    babelify   = require('babelify'),
-    gbrowserify = require('gulp-browserify');
+var gulp = require('gulp');
+var path = require('path');
+var fs = require('fs');
+var concat = require('gulp-concat');
+var transform = require('vinyl-transform');
+var glob = require('glob');
+var webpack_stream = require('webpack-stream');
+var webpack2 = require('webpack');
 
 
-gulp.task('buildapp', function (cb) {
-    gulp.src('./src/js/*.js')
-        .pipe(gbrowserify({
-            transform: ['babelify'],
-        }))
-        .pipe(gulp.dest('dist/js/'))
-        .on('end', function() { console.log('transpile succeed.'); cb();})
+var vendors = [
+    'node_modules/proptypes/index.js',
+    'node_modules/react/dist/react-with-addons.min.js',
+    'node_modules/react-dom/dist/react-dom.min.js'
+];
+
+
+var appDependencies = [
+    'Resources/public/js/utils/*.js',
+    'Resources/public/js/app.js'
+];
+
+var compile = function(entry, dest) {
+    return gulp.src(entry)
+        //.pipe(named())
+        .pipe(webpack_stream({
+            cache: true,
+            module: {
+                rules: [
+                    {
+                        test: /\.(js|jsx)$/,
+                        exclude: /node_modules/,
+                        use: [
+                            'babel-loader'
+                        ]
+                    }
+                ]
+            },
+            resolve: {
+              unsafeCache:true
+            },
+            plugins: [
+                new webpack2.LoaderOptionsPlugin({
+                    minimize: true,
+                    debug: false
+                }),
+                new webpack2.optimize.UglifyJsPlugin({
+                    compress: {
+                        warnings: false,
+                        screw_ie8: true,
+                        conditionals: true,
+                        unused: true,
+                        comparisons: true,
+                        sequences: true,
+                        dead_code: true,
+                        evaluate: true,
+                        if_return: true,
+                        join_vars: true,
+                    },
+                    output: {
+                        comments: false,
+                    },
+                })
+            ],
+        }, webpack2))
+        .pipe(gulp.dest(dest));
+};
+
+
+//combined and concat main js in
+gulp.task('combine:vendors', [], function () {
+    return gulp.src(vendors)
+        .pipe(concat('vendors-combined.js'))
+        .pipe(gulp.dest('./dist/'))
+        .on('end', function() {
+            console.log('Concat main javascripts done.');
+        });
 });
 
-gulp.task('watchapp', function() {
-    gulp.watch(['./src/js/*.js', './src/js/*/*.js'], ['buildapp']);
+gulp.task('build:app', function () {
+    return compile('./src/js/bootstrap.js', './Resources/public/js/dist/');
 });

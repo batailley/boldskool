41c643a627df8e226f23b78ad0f79e69a8cec0c9
laurentbatailley <laurent.batailley@gmail.com>
2017-04-05 23:05:39 +0200

    dependencyManager tests wip

--- tests/DependencyManager-test.js
+++ tests/DependencyManager-test.js
@@ -1,11 +1,24 @@
-import { expect } from 'chai'
+import chai from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+
+chai.should()
+chai.use(sinonChai)
+const expect = chai.expect
 
 import DependencyManager from '../src/js/utils/DependencyManager'
 
 describe('DependencyManager Tests', () => {
   it('DependencyManager should callback when dep is ready', () => {
-    let dpMan = new DependencyManager(['chaussette'])
+    let depReady = ''
+    let depsReferences = ['salade', 'tomates', 'oignons']
+    let dpMan = new DependencyManager(depsReferences)
+    let callback = sinon.spy()
 
-    expect(1 + 1).to.equal(expectedResult)
+    dpMan.dependenciesListener(['tomates'], callback)
+
+    dpMan.dispatch('tomates-ready')
+
+    expect(callback).to.have.been.called
   })
 })
